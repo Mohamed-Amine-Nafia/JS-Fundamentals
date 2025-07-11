e1477b08c9ec6c20c8515a090288828e032b924f
Mohamed Amine Nafia <m.amine.nafia@gmail.com>
2025-07-11 20:09:44 +0200

    fix the code

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,4 +1,9 @@
-let i = 0;
-for (let i = 0; i < 5; i++) {
-  console.log("C is fun");
+const count = parseInt(process.argv[2]);
+
+if (isNaN(count)) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < count; i++) {
+    console.log("C is fun");
+  }
 }
